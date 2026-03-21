ae3cf5b4429df4a77616bb0d9cf1cfd9f3b2ec77
Chris <chrisroberts1080@gmail.com>
2026-03-21 11:26:45 +0000

    Increase complexity of benchmark

--- benches/renderer_bench.rs
+++ benches/renderer_bench.rs
@@ -1,17 +1,27 @@
 /// To measure if any code improvements improved render time
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use rust_renderer::create_scene;
+use rust_renderer::{create_complex_scene, create_simple_scene};
 
-fn bench_render(c: &mut Criterion) {
-    let (mut scene, _update_handle, _update_running) = create_scene().unwrap();
+fn bench_render_simple_scene(c: &mut Criterion) {
+    let (mut scene, _update_handle, _update_running) = create_simple_scene().unwrap();
 
-    c.bench_function("render", |b| {
+    c.bench_function("render_simple", |b| {
         b.iter(|| {
             scene.render_scene();
         })
     });
 }
 
-criterion_group!(benches, bench_render);
+fn bench_render_complex_scene(c: &mut Criterion) {
+    let (mut scene, _update_handle, _update_running) = create_complex_scene().unwrap();
+
+    c.bench_function("render_complex", |b| {
+        b.iter(|| {
+            scene.render_scene();
+        })
+    });
+}
+
+criterion_group!(benches, bench_render_simple_scene, bench_render_complex_scene);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -20,18 +20,66 @@ use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::thread::JoinHandle;
 
-/// Creates a default scene
+/// Creates a simple scene with one monkey and 3 lights
 ///
 /// Returns a scene a handle for the object update thread and a bool for if that thread is running
-pub fn create_scene() -> Result<(Scene, JoinHandle<()>, Arc<AtomicBool>), Box<dyn std::error::Error>>
+pub fn create_simple_scene() -> Result<(Scene, JoinHandle<()>, Arc<AtomicBool>), Box<dyn std::error::Error>>
 {
-    let monkey = Object::new(
-        ObjLoader::load(Path::new("monkey.obj"))?,
-        Transform::default(),
-        Material::Color([255, 255, 255, 255]),
-    );
+    let scene_objects = vec![
+        Object::new(
+            ObjLoader::load(Path::new("monkey.obj"))?,
+            Transform::default(),
+            Material::Color([255, 255, 255, 255]),
+        ),
+    ];
 
-    let scene_objects = vec![monkey];
+    let scene_lights = vec![
+        PointLight::new(Vec3::new(0.0, 0.0, 5.0), [1.0, 0.0, 0.0], 15.0),
+        PointLight::new(Vec3::new(5.0, 0.0, 0.0), [0.0, 1.0, 0.0], 20.0),
+        PointLight::new(Vec3::new(-5.0, 0.0, 0.0), [0.0, 0.0, 1.0], 15.0),
+    ];
+
+    let mut scene = Scene::new(800.0, 600.0, scene_objects, scene_lights);
+
+    scene.skybox = Texture::load(Path::new("test.png")).ok();
+
+    let (update_handle, update_running) = scene.spawn_update_thread();
+
+    Ok((scene, update_handle, update_running))
+}
+
+
+/// Creates a "complex" scene which involves 4 monkeys and 3 lights
+pub fn create_complex_scene() -> Result<(Scene, JoinHandle<()>, Arc<AtomicBool>), Box<dyn std::error::Error>>
+{
+    let scene_objects = vec![
+        Object::new(
+            ObjLoader::load(Path::new("monkey.obj"))?,
+            Transform::default(),
+            Material::Color([255, 255, 255, 255]),
+        ),
+        Object::new(
+            ObjLoader::load(Path::new("monkey.obj"))?,
+            Transform::in_position(
+                Vec3::new(0.0, 5.0, 0.0)
+            ),
+            Material::Color([255, 255, 255, 255]),
+        ),
+        Object::new(
+            ObjLoader::load(Path::new("monkey.obj"))?,
+            Transform::in_position(
+                Vec3::new(5.0, 0.0, 0.0)
+            ),
+            Material::Color([255, 255, 255, 255]),
+        ),
+        Object::new(
+            ObjLoader::load(Path::new("monkey.obj"))?,
+            Transform::in_position(
+                Vec3::new(0.0, 0.0, -5.0)
+            ),
+            Material::Color([255, 255, 255, 255]),
+        ),
+    ];
 
     let scene_lights = vec![
         PointLight::new(Vec3::new(0.0, 0.0, 5.0), [1.0, 0.0, 0.0], 15.0),
