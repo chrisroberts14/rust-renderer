6cac241a3d902416a0df021a6b08d68ee73e8b91
Chris Roberts <chris.roberts@ditto.com>
2026-03-27 19:51:40 +0000

    Remove unneeded functions in overlay

--- src/overlay.rs
+++ src/overlay.rs
@@ -8,18 +8,8 @@ pub struct StatsOverlay {
 }
 
 impl StatsOverlay {
-    pub fn new() -> Self {
-        Self {
-            stats: HashMap::new(),
-        }
-    }
-
     pub fn add(&mut self, key: &str, value: &str) {
         self.stats.insert(key.to_string(), value.to_string());
-    }
-
-    pub fn stat(&self, key: &str) -> Option<&String> {
-        self.stats.get(key)
     }
 
     pub fn write_to_framebuffer(&self, framebuffer: &mut Framebuffer) {
