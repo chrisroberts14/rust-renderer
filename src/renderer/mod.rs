6e5d3b54a808072b391f8ca0f0c9bcb36b41cdda
Chris <chrisroberts1080@gmail.com>
2026-03-30 20:12:45 +0100

    Disable unused variable lint

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -88,12 +88,14 @@ pub trait Renderer {
 
     /// Increase the number of tiles
     /// Is a no-op if the renderer is not tile based
+    #[allow(unused_variables)]
     fn increase_tile_count(&mut self, delta: usize) {
         ()
     }
 
     /// Decrease the number of tiles
     /// Is a no-op if the renderer is not tile based
+    #[allow(unused_variables)]
     fn decrease_tile_count(&mut self, delta: usize) {
         ()
     }
