8bb09c2fee8d2bef13cee7195a0112b30da9cd47
Chris <chrisroberts1080@gmail.com>
2026-04-03 13:37:49 +0100

    Add height and width as args

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ struct Args {
     /// Which renderer to use
     #[arg(long, value_enum, default_value_t = RendererChoice::Gpu)]
     renderer: RendererChoice,
+
+    /// Width of the window to create
+    #[arg(long, default_value_t = 800.0)]
+    width: f32,
+
+    /// Height of the window to create
+    #[arg(long, default_value_t = 600.0)]
+    height: f32,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,7 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event_loop = EventLoop::new()?;
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    let app = App::new(None, renderer, 800.0, 600.0)?;
+    let app = App::new(None, renderer, args.width, args.height)?;
 
     event_loop.run_app(app)?;
 
